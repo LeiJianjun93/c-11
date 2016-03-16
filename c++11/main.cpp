139fb9947e78d0e2c11b2511db1bfe92f2cec435
student <student@ssssss.local>
2016-03-16 09:32:10 +0800

    Initial Commit

---
+++ c++11/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c++11
+//
+//  Created by student on 16/3/16.
+//  Copyright © 2016年 student. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
